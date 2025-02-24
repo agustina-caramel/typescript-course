9988d539d17fad33c1c46d4bc7905db69642d965
agustina-caramel <agus@192.168.1.6>
2025-02-24 17:18:46 -0300

    Update file organization using ES modules

---
+++ src/decorators/autobind-decorator.ts
@@ -0,0 +1,11 @@
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDecorator: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDecorator;
+}
